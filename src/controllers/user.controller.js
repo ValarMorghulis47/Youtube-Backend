d102dcf950f27303624f2b7cc6e93181cb18ae76
Muhammad Yasir <myasir47991@gmail.com>
2024-02-17 17:21:29 +0500

    Remove commented code and unnecessary comments in user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,15 +27,6 @@ const generateTokens = async (userId) => {
 }
 
 const registerUser = asyncHandler(async (req, res) => {
-    // get user details from frontend
-    // validation - not empty
-    // check if user already exists: username, email
-    // check for images, check for avatar
-    // upload them to cloudinary, avatar
-    // create user object - create entry in db
-    // remove password and refresh token field from response
-    // check for user creation
-    // return res
     const { fullname, username, email, password } = req.body;
     if ([fullname, username, email, password].some((field) => field?.trim() === "")) {
         throw new ApiError(400, "All Fields Are Required");
